ae518f0ef5848a8cd81e08c48ab437af7d4321db
home <catalinpce@gmail.com>
2023-09-15 09:41:14 +0300

    added router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Layout from 'layout/layout'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from 'pages/home/home'
 
 export default function App() {
   return (
     <Layout>
-      <label>Hello World</label>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </Layout>
   )
 }
